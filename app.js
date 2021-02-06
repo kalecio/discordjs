1fd28c04c69bd93ec5557cb8d870fda692d838a0
kalecio <kalecio.santana@gmail.com>
2021-02-06 18:02:53 -0300

    fix: get env info from .env file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json')
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
 // PING
 client.on('message', async msg => {
-  if (msg.content === `${config.prefix}ping`) {
-    msg.reply(`pong!`);
+  if (msg.content === `${process.env.PREFIX}ping`) {
+    msg.reply(`pong!`)
   }
-});
+})
 
-client.login(config.token);
+client.login(process.env.API_TOKEN);
